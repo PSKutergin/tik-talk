8a5827bcc809a4f7b5c31defc76d771261be05e2
PSKutergin <PSKutergin@gmail.com>
2025-02-17 11:21:57 +0100

    added btns send-message and link to profile

--- libs/profile/src/lib/ui/profile-card/profile-card.component.ts
+++ libs/profile/src/lib/ui/profile-card/profile-card.component.ts
@@ -1,14 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { AvatarCircleComponent } from '@tt/common';
 import { Profile } from '@tt/interfaces/profile';
 
 @Component({
   selector: 'profile-card',
   standalone: true,
-  imports: [AvatarCircleComponent],
+  imports: [AvatarCircleComponent, RouterLink],
   templateUrl: './profile-card.component.html',
   styleUrl: './profile-card.component.scss'
 })
 export class ProfileCardComponent {
+  private router = inject(Router);
   @Input() profile!: Profile;
+
+  async onSendMessage(userId: number) {
+    this.router.navigate(['/chats', 'new'], { queryParams: { userId } });
+  }
 }
